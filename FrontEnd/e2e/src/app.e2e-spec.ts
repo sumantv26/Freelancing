47a47a783156b2231ac861c047da74aef3c070c2
sumant <sumantv26@gmail.com>
2020-06-15 12:15:41 +0530

    login added

--- FrontEnd/e2e/src/app.e2e-spec.ts
+++ FrontEnd/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('freelancing app is running!');
+    expect(page.getTitleText()).toEqual('FrontEnd app is running!');
   });
 
   afterEach(async () => {
--- FrontEnd/karma.conf.js
+++ FrontEnd/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/freelancing'),
+      dir: require('path').join(__dirname, './coverage/FrontEnd'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
